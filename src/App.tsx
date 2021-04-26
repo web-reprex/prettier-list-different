70d96eeea558fcfdb6b67358cf5c7646331a543e
Seyyed Mahdi Hassanpour <seyyedkhandon@gmail.com>
2021-04-26 14:40:44 +0430

    feat: add typed @reduxjs/toolkit boiler plate for counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
